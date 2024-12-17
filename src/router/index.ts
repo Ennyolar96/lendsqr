27e69d1dc451a87c600598e80131363210bea763
ennyolar96 <ennyolar1996@gmail.com>
2024-12-17 19:52:44 +0100

    completed

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,10 @@
-import { Application } from "express";
+import { Application, Request, Response } from "express";
 import authRouter from "./auth";
 import userRouter from "./user";
 import walletRouter from "./wallet";
 
 const ApplicationRoutes = (app: Application) => {
-  app.get("/", (req, res) => {
+  app.get("/", (req: Request, res: Response) => {
     res.send("Hello, World!");
   });
   app.use("/api/v1/auth/", authRouter);
